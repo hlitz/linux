ca1aa0b47f15500667f433a6624417548159e1b6
Javier González <javier@cnexlabs.com>
2018-02-19 02:36:03 +0100

    lightnvm: pblk: check for supported version
    At this point, only 1.2 spec is supported, thus check for it. Also,
    since device-side L2P is only supported in the 1.2 spec, make sure to
    only check its value under 1.2.
    
    Signed-off-by: Javier González <javier@cnexlabs.com>

--- drivers/lightnvm/pblk-init.c
+++ drivers/lightnvm/pblk-init.c
@@ -990,9 +990,15 @@ static void *pblk_init(struct nvm_tgt_dev *dev, struct gendisk *tdisk,
 	struct pblk *pblk;
 	int ret;
 
-	if (dev->geo.c.dom & NVM_RSP_L2P) {
+	if (geo->c.version != NVM_OCSSD_SPEC_12) {
+		pr_err("pblk: OCSSD version not supported (%u)\n",
+							geo->c.version);
+		return ERR_PTR(-EINVAL);
+	}
+
+	if (geo->c.version == NVM_OCSSD_SPEC_12 && geo->c.dom & NVM_RSP_L2P) {
 		pr_err("pblk: host-side L2P table not supported. (%x)\n",
-							dev->geo.c.dom);
+							geo->c.dom);
 		return ERR_PTR(-EINVAL);
 	}
 
